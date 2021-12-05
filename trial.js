a912d0a187145b39c3d93c5bc7b06162611afb91
Ali Alaeddine <a.alaeddine@live.com>
2021-12-05 17:07:50 +0100

    ex.2 solved

---
+++ trial.js
@@ -0,0 +1,9 @@
+
+
+let array = [200,90,70,55,23,56,450,1,31,79,68,90]
+let minimum = array[0]
+for (i=0; i<array.length; i++){
+   
+    if(array[i]<minimum){ minimum = array[i]
+    } 
+}   console.log(minimum)
